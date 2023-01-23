8a9a0bd2cbeba704a9fea0bdd7a9f8927164343c
Hamaad Siddiqui <hamaad_siddiqui@hotmail.com>
2023-01-24 03:33:44 +0500

    fix validation (again)

--- pages/api/revalidate.ts
+++ pages/api/revalidate.ts
@@ -1,11 +1,17 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
+import { isValidSignature, SIGNATURE_HEADER_NAME } from '@sanity/webhook';
 
 export default async function handler(
  req: NextApiRequest,
  res: NextApiResponse
 ) {
- if (req.query.secret !== process.env.SANITY_STUDIO_REVALIDATE_SECRET) {
-  return res.status(401).json({ message: 'Invalid token' });
+ const body = await readBody(req);
+ const signature = req.headers[SIGNATURE_HEADER_NAME] as string;
+ const secret = process.env.SANITY_STUDIO_REVALIDATE_SECRET;
+
+ if (!isValidSignature(body, signature, secret)) {
+  res.status(401).json({ message: 'Invalid signature' });
+  return;
  }
 
  try {
@@ -14,4 +20,12 @@ export default async function handler(
  } catch (err) {
   return res.status(500).send('Error revalidating');
  }
+}
+
+async function readBody(readable: NextApiRequest) {
+ const chunks = [];
+ for await (const chunk of readable) {
+  chunks.push(typeof chunk === 'string' ? Buffer.from(chunk) : chunk);
+ }
+ return Buffer.concat(chunks).toString('utf8');
 }
