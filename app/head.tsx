53ff55717c51ebe49c01563b5fb8806ea68ab11d
Hamaad Siddiqui <hamaad.siddiqui404@gmail.com>
2024-06-19 02:17:35 +0500

    feat: new metadata

--- app/head.tsx
+++ app/head.tsx
@@ -1,42 +1,42 @@
 export default function Head() {
- return (
-  <>
-   <title>Hamaad Siddiqui</title>
-   <meta content='width=device-width, initial-scale=1' name='viewport' />
-   <meta
-    name='description'
-    content='Full-Stack Developer building intuitive and efficient applications using React / Next.js & Flutter. Passionate about staying current with industry developments and continuously expanding my skillset to deliver innovative solutions.'
-   />
-   <link href='/static/favicons/favicon.ico' rel='shortcut icon' />
-   <link href='/static/favicons/site.webmanifest' rel='manifest' />
-   <link
-    href='/static/favicons/apple-touch-icon.png'
-    rel='apple-touch-icon'
-    sizes='180x180'
-   />
-   <link
-    href='/static/favicons/favicon-32x32.png'
-    rel='icon'
-    sizes='32x32'
-    type='image/png'
-   />
-   <link
-    href='/static/favicons/favicon-16x16.png'
-    rel='icon'
-    sizes='16x16'
-    type='image/png'
-   />
-   <link
-    color='#5bbad5'
-    href='/static/favicons/safari-pinned-tab.svg'
-    rel='mask-icon'
-   />
-   <meta content='#ffffff' name='theme-color' />
-   <meta content='#ffffff' name='msapplication-TileColor' />
-   <meta
-    content='/static/favicons/browserconfig.xml'
-    name='msapplication-config'
-   />
-  </>
- );
+  return (
+    <>
+      <title>Hamaad Siddiqui</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta
+        name="description"
+        content="Full-Stack Developer excelling in crafting intuitive, efficient applications across industries. Passionate about staying current with industry developments and continuously expanding my skillset to deliver innovative solutions."
+      />
+      <link href="/static/favicons/favicon.ico" rel="shortcut icon" />
+      <link href="/static/favicons/site.webmanifest" rel="manifest" />
+      <link
+        href="/static/favicons/apple-touch-icon.png"
+        rel="apple-touch-icon"
+        sizes="180x180"
+      />
+      <link
+        href="/static/favicons/favicon-32x32.png"
+        rel="icon"
+        sizes="32x32"
+        type="image/png"
+      />
+      <link
+        href="/static/favicons/favicon-16x16.png"
+        rel="icon"
+        sizes="16x16"
+        type="image/png"
+      />
+      <link
+        color="#000000"
+        href="/static/favicons/safari-pinned-tab.svg"
+        rel="mask-icon"
+      />
+      <meta content="#ffffff" name="theme-color" />
+      <meta content="#ffffff" name="msapplication-TileColor" />
+      <meta
+        content="/static/favicons/browserconfig.xml"
+        name="msapplication-config"
+      />
+    </>
+  );
 }
