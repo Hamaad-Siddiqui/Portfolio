6c93060b7f497e55fb79b18c250250e6a500e670
Hamaad Siddiqui <hamaad.siddiqui404@gmail.com>
2024-06-18 19:05:37 +0500

    chore: upgrade issues

--- pages/api/revalidate.ts
+++ pages/api/revalidate.ts
@@ -1,38 +1,38 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
-import { isValidSignature, SIGNATURE_HEADER_NAME } from '@sanity/webhook';
+import type { NextApiRequest, NextApiResponse } from "next";
+import { isValidSignature, SIGNATURE_HEADER_NAME } from "@sanity/webhook";
 
 export default async function handler(
- req: NextApiRequest,
- res: NextApiResponse
+  req: NextApiRequest,
+  res: NextApiResponse
 ) {
- const body = await readBody(req);
- const signature = req.headers[SIGNATURE_HEADER_NAME] as string;
- const secret = process.env.SANITY_STUDIO_REVALIDATE_SECRET;
+  const body = await readBody(req);
+  const signature = req.headers[SIGNATURE_HEADER_NAME] as string;
+  const secret = process.env.SANITY_STUDIO_REVALIDATE_SECRET as string;
 
- if (!isValidSignature(body, signature, secret)) {
-  res.status(401).json({ message: 'Invalid signature' });
-  return;
- }
+  if (!isValidSignature(body, signature, secret)) {
+    res.status(401).json({ message: "Invalid signature" });
+    return;
+  }
 
- try {
-  await res.revalidate('/');
-  res.status(200).json({ message: 'Revalidated' });
-  return;
- } catch (err) {
-  return res.status(500).send('Error revalidating');
- }
+  try {
+    await res.revalidate("/");
+    res.status(200).json({ message: "Revalidated" });
+    return;
+  } catch (err) {
+    return res.status(500).send("Error revalidating");
+  }
 }
 
 export const config = {
- api: {
-  bodyParser: false,
- },
+  api: {
+    bodyParser: false,
+  },
 };
 
 async function readBody(readable: NextApiRequest) {
- const chunks = [];
- for await (const chunk of readable) {
-  chunks.push(typeof chunk === 'string' ? Buffer.from(chunk) : chunk);
- }
- return Buffer.concat(chunks).toString('utf8');
+  const chunks: any[] = [];
+  for await (const chunk of readable) {
+    chunks.push(typeof chunk === "string" ? Buffer.from(chunk) : chunk);
+  }
+  return Buffer.concat(chunks).toString("utf8");
 }
