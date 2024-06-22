4a0fef4b0ca2206a82de72efbb8e161d31dc9b29
Hamaad Siddiqui <hamaad.siddiqui404@gmail.com>
2024-06-23 01:21:55 +0500

    fix: padding + hover

--- components/Container.tsx
+++ components/Container.tsx
@@ -8,7 +8,7 @@ export default function Container(props: { children: React.ReactNode }) {
       <div className="mx-auto min-w-[20rem] max-w-[80rem] mb-10 px-4 sm:px-6">
         <div className="flex flex-row justify-end w-full p-5">=</div>
         <div className="flex flex-col justify-center w-full">{children}</div>
-        <div className="flex w-full items-center justify-between mt-[7rem] text-primary">
+        <div className="flex w-full items-center justify-between mt-[5rem] text-primary">
           <div className="flex items-center gap-1">
             <CCIcon />
             <div className="max-[550px]:hidden">2024, Hamaad</div>
@@ -18,9 +18,9 @@ export default function Container(props: { children: React.ReactNode }) {
             target="_blank"
             rel="noreferrer"
           >
-            <div className="flex items-center gap-1">
+            <div className="flex items-center gap-1 hover:text-blue-400">
               <ForkIcon />{" "}
-              <div className="max-[450px]:hidden">
+              <div className="max-[450px]:hidden ">
                 Made with &lt;3! in Next.js, Tailwind & Sanity
               </div>
             </div>
